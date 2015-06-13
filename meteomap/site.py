3cf71c66b224368849da27828c44348555f78ae3
Simon Lemieux <lemieux.simon@gmail.com>
2015-06-13 10:08:47 -0400

    we now have a geo query

--- meteomap/site.py
+++ meteomap/site.py
@@ -1,10 +1,11 @@
-import sys, json
+import sys, json, argparse
 
 from flask import Flask, request
-from sqlalchemy import desc
+from sqlalchemy import desc, func
+from geoalchemy2.elements import WKTElement
 
 from meteomap.tables import City
-from meteomap.utils import init_session
+from meteomap.utils import init_session, configure_logging
 
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_route():
-    return '<a href="data?n=46&s=45&w=-74&e=74">data</a>'
+    return '<a href="data?n=46&s=45&w=-73&e=-74">Montreal</a>'
 
 @app.route('/data')
 def data_route():
@@ -25,6 +26,13 @@ def data_route():
 
     session = init_session()
     data = session.query(City.name, City.population) \
+        .filter(
+            func.ST_Covers(
+                WKTElement(
+                    'POLYGON(({0} {1}, {0} {2}, {3} {2}, {3} {1}, {0} {1}))'
+                    .format(south, east, west, north),
+                    srid=4326),
+                City.location)) \
         .order_by(desc(City.population)) \
         .limit(100).all()
     data = [x._asdict() for x in data]
@@ -32,8 +40,10 @@ def data_route():
 
 
 if __name__ == '__main__':
-    # arg1 : --debug
-    debug = False
-    if len(sys.argv) > 1 and sys.argv[1] == '--debug':
-        debug = True
-    debug = app.run(debug=debug)
+    parser = argparse.ArgumentParser(
+        description='Start the server.')
+    parser.add_argument('--debug', action='store_true',
+                        help='Run the server in debug mode')
+    args = parser.parse_args()
+    configure_logging()
+    app.run(debug=args.debug)
